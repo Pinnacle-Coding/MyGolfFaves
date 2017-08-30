94f4c1e0d15c160fe30c719511cd8311e85cf1d6
dmhacker <dmhacker@yahoo.com>
2017-08-30 13:52:35 -0700

    Push old files to new

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View} from 'react-native';
+import { Router, Route, Switch } from 'react-router-native';
 
-export default class App extends React.Component {
+import Register from './src/components/Register.js';
+import Login from './src/components/Login.js';
+import Home from './src/components/Home.js';
+import Profile from './src/components/Profile.js';
+import Rewards from './src/components/Rewards.js';
+import FavoriteGolfCourses from './src/components/FavoriteGolfCourses.js';
+import Offers from './src/components/Offers.js';
+import Offer from './src/components/Offer.js';
+import Wallet from './src/components/Wallet.js';
+import Redeem from './src/components/Redeem.js';
+import Notifications from './src/components/Notifications.js';
+
+import history from './src/utils/history.js';
+
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Router history={history}>
+        <Switch>
+          <Route path="/login" component={Login}/>
+          <Route path="/register" component={Register}/>
+          <Route path="/home" component={Home}/>
+          <Route path="/profile" component={Profile}/>
+          <Route path="/favoriteCourses" component={FavoriteGolfCourses}/>
+          <Route path="/offers" component={Offers}/>
+          <Route path="/offer" component={Offer}/>
+          <Route path="/wallet" component={Wallet}/>
+          <Route path="/redeem" component={Redeem}/>
+          <Route path="/rewards" component={Rewards}/>
+          <Route path="/notifications" component={Notifications}/>
+        </Switch>
+      </Router>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
